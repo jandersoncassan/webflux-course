8d78a7494fc24e1dbfd15da22078151dfa864682
Janderson Cassan <jande.max@gmail.com>
2024-01-09 19:53:12 -0300

    add new class validation error binding validation jakarta

--- src/main/java/br/com/jande/webfluxcourse/controller/exceptions/ControllerExceptionHandler.java
+++ src/main/java/br/com/jande/webfluxcourse/controller/exceptions/ControllerExceptionHandler.java
@@ -3,8 +3,10 @@ package br.com.jande.webfluxcourse.controller.exceptions;
 import org.springframework.dao.DuplicateKeyException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.support.WebExchangeBindException;
 import reactor.core.publisher.Mono;
 
 import java.time.LocalDateTime;
@@ -21,12 +23,31 @@ public class ControllerExceptionHandler {
         return ResponseEntity.badRequest()
                 .body(Mono.just(StandardError.builder()
                                 .timestamp(LocalDateTime.now())
+                                .path(request.getPath().toString())
                                 .status(BAD_REQUEST.value())
                                 .error(BAD_REQUEST.getReasonPhrase())
                                 .message(verifyMessageException(ex.getMessage()))
-                                .path(request.getPath().toString())
+                                .build()));
+    }
 
-                        .build()));
+    @ExceptionHandler(WebExchangeBindException.class)
+    public ResponseEntity<Mono<ValidationError>> validationError(
+            WebExchangeBindException ex, ServerHttpRequest request
+    ){
+        ValidationError error = new ValidationError(
+                LocalDateTime.now(),
+                request.getPath().toString(),
+                BAD_REQUEST.value(),
+               "Validation Error",
+               "Error validation attributes"
+        );
+
+        for(FieldError x: ex.getBindingResult().getFieldErrors()){
+            error.addError(x.getField(), x.getDefaultMessage());
+        }
+
+        return ResponseEntity.badRequest()
+                .body(Mono.just(error));
     }
 
     private String verifyMessageException(final String message){
--- src/main/java/br/com/jande/webfluxcourse/model/request/UserRequest.java
+++ src/main/java/br/com/jande/webfluxcourse/model/request/UserRequest.java
@@ -1,8 +1,18 @@
 package br.com.jande.webfluxcourse.model.request;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public record UserRequest(
+        @NotBlank(message = "must not be null or empty")
+        @Size(min = 3, max = 50, message = "must be between 3 and 50 characters")
         String name,
+        @NotBlank(message = "must not be null or empty")
+        @Email(message = "invalid email")
         String email,
+        @NotBlank(message = "must not be null or empty")
+        @Size(min = 3, max = 50, message = "must be between 3 and 50 characters")
         String password
 ) {
 }
