98cb0740214407bc4eefe2487f605ca508a162e3
Janderson Cassan <jande.max@gmail.com>
2024-01-09 19:02:53 -0300

    add mapper to user and impl to save contract user contract

---
+++ src/main/java/br/com/jande/webfluxcourse/mapper/UserMapper.java
@@ -0,0 +1,20 @@
+package br.com.jande.webfluxcourse.mapper;
+
+import br.com.jande.webfluxcourse.entity.User;
+import br.com.jande.webfluxcourse.model.request.UserRequest;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import static org.mapstruct.NullValueCheckStrategy.ALWAYS;
+import static org.mapstruct.NullValuePropertyMappingStrategy.IGNORE;
+
+
+@Mapper(
+        componentModel = "spring",
+        nullValuePropertyMappingStrategy = IGNORE,
+        nullValueCheckStrategy = ALWAYS
+)
+public interface UserMapper {
+
+    @Mapping(target = "id", ignore = true)
+    User toEntity(final UserRequest request);
+}
---
+++ src/main/java/br/com/jande/webfluxcourse/service/UserService.java
@@ -0,0 +1,20 @@
+package br.com.jande.webfluxcourse.service;
+
+import br.com.jande.webfluxcourse.entity.User;
+import br.com.jande.webfluxcourse.mapper.UserMapper;
+import br.com.jande.webfluxcourse.model.request.UserRequest;
+import br.com.jande.webfluxcourse.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository repository;
+    private final UserMapper mapper;
+    public Mono<User> save(final UserRequest request){
+        return repository.save(mapper.toEntity(request));
+    }
+}
