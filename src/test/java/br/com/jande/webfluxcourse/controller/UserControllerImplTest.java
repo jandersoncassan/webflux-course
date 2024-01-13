55c09a62d966e65dd5686ae7cd677504dc341f7b
Janderson Cassan <jande.max@gmail.com>
2024-01-13 09:23:04 -0300

    add integration test endpoint save with bad request

--- src/test/java/br/com/jande/webfluxcourse/controller/UserControllerImplTest.java
+++ src/test/java/br/com/jande/webfluxcourse/controller/UserControllerImplTest.java
@@ -15,15 +15,18 @@ import org.springframework.boot.test.autoconfigure.web.client.AutoConfigureWebCl
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.reactive.function.BodyInserters;
 import reactor.core.publisher.Mono;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
@@ -35,12 +38,6 @@ class UserControllerImplTest {
 
     @MockBean
     private UserService service;
-
-    @MockBean
-    private UserMapper mapper;
-
-    @MockBean
-    private MongoClient mongoClient;
 
     @Test
     @DisplayName("Test endpoint save with success")
@@ -56,6 +53,27 @@ class UserControllerImplTest {
                 .expectStatus().isCreated();
 
         verify(service, times(1)).save(any(UserRequest.class));
+    }
+
+    @Test
+    @DisplayName("Test endpoint save with Bad Request")
+    void testSaveWithNameBadRequest() {
+        final var request = new UserRequest(" Jande", "jande.max@teste.com.br", "123456");
+
+       webTestClient.post()
+                .uri("/users")
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(BodyInserters.fromValue(request))
+                .exchange()
+                .expectStatus().isBadRequest()
+               .expectBody()
+               .jsonPath("$.path").isEqualTo("/users")
+               .jsonPath("$.status").isEqualTo(BAD_REQUEST.value())
+               .jsonPath("$.error").isEqualTo("Validation Error")
+               .jsonPath("$.message").isEqualTo("Error validation attributes")
+               .jsonPath("$.errors[0].fieldName").isEqualTo("name")
+                .jsonPath("$.errors[0].message").isEqualTo("field cannot have blank spaces at the beginning or at end");
+
     }
 
     @Test
